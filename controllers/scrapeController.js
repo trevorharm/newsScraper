f5500e08b30b54796711bcce3b70eaa27fe54016
Trevor Harm <trevorharm@users.noreply.github.com>
2018-04-25 14:27:06 -0400

    2

---
+++ controllers/scrapeController.js
@@ -0,0 +1,53 @@
+// Dependencies Defined
+var db = require("../models");
+var exports = module.exports = {};
+
+exports.scrape= function(req, res){
+
+ // First, we grab the body of the html with request
+ axios.get("https://www.popsci.com//").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
+  })
+};
+
+exports.articles = function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
---
+++ server.js
@@ -0,0 +1,41 @@
+// Dependencies Defined
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars")
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+//For Handlebars
+app.set('views', './views')
+app.engine('hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/newsScraper_DB");
+
+// Routes
+require("./routes/route.js");
+
+
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
